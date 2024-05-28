41962fc9938d815fcc47f66f68c9c16c5d01d7e9
PedroOPozo96 <168337714+PedroOPozo96@users.noreply.github.com>
2024-05-28 04:28:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -126,4 +126,5 @@ def detalle_libro(id):
         return render_template('index.html', error=error)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
